8c095ffae7eebecae94b53c9db84be71816f7d4e
Andy Vey <avey@udel.edu>
2025-03-20 22:36:02 -0400

    completed rest of forms

--- src/form-components/ChangeColor.tsx
+++ src/form-components/ChangeColor.tsx
@@ -1,9 +1,48 @@
 import React, { useState } from "react";
 
 export function ChangeColor(): React.JSX.Element {
+    const colors = [
+        "red",
+        "blue",
+        "green",
+        "yellow",
+        "orange",
+        "purple",
+        "pink",
+        "brown",
+    ];
+    const [selected, setSelected] = useState<string>(colors[0]);
     return (
         <div>
             <h3>Change Color</h3>
+            {colors.map((color) => (
+                <label key={color} style={{ marginRight: "10px" }}>
+                    <input
+                        type="radio"
+                        value={color}
+                        name="color"
+                        checked={selected === color}
+                        onChange={(e) => {
+                            setSelected(e.target.value);
+                        }}
+                    />
+                    {color}
+                </label>
+            ))}
+            <div
+                data-testid="colored-box"
+                style={{
+                    backgroundColor: selected,
+                    color: "white",
+                    padding: "10px",
+                    marginTop: "10px",
+                    border: "1px solid black",
+                    width: "150px",
+                    textAlign: "center",
+                }}
+            >
+                {selected}
+            </div>
         </div>
     );
 }
--- src/form-components/EditMode.tsx
+++ src/form-components/EditMode.tsx
@@ -1,9 +1,49 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap"; // Import Form for Bootstrap Switc
 
 export function EditMode(): React.JSX.Element {
+    const [editing, setEditing] = useState<boolean>(false);
+    const [name, setName] = useState<string>("Your Name");
+    const [isStudent, setStudent] = useState<boolean>(true);
+
     return (
         <div>
             <h3>Edit Mode</h3>
+            <Form.Check
+                type="switch"
+                id="edit-mode-switch"
+                label="Edit Mode"
+                checked={editing}
+                onChange={(e) => {
+                    setEditing(e.target.checked);
+                }}
+            />
+            {editing ?
+                <div>
+                    <Form.Group controlId="nameInput">
+                        <Form.Label>Name:</Form.Label>
+                        <Form.Control
+                            type="text"
+                            value={name}
+                            onChange={(e) => {
+                                setName(e.target.value);
+                            }}
+                        />
+                    </Form.Group>
+                    <Form.Check
+                        type="checkbox"
+                        id="is-student-check"
+                        label="Student"
+                        checked={isStudent}
+                        onChange={(e) => {
+                            setStudent(e.target.checked);
+                        }}
+                    />
+                </div>
+            :   <p>
+                    {name} is {isStudent ? "a student" : "not a student"}.{" "}
+                </p>
+            }
         </div>
     );
 }
